1aee7cec4f88ec6211f5089445bed90d2d5987d2
Hasibur Rahman <hasiburdev@gmail.com>
2022-09-25 02:21:36 +0600

    Create test.php

---
+++ php/test.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Using constant.
+define("PI", 3.14159);
+
+$name = "PHP";
+$age = 23;
+
+echo $name;
+echo "\n";
+// echo PI;
+// printing constant
+echo constant("PI");
+echo "\n";
+
+
+echo "Hello";
