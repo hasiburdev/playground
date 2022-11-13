d33e72bdd81ef96cf8555c243728953594b3a528
Hasibur Rahman <hasiburdev@gmail.com>
2022-11-14 01:23:12 +0600

    Add type guard in typescript

---
+++ typescript/examples/sort/src/index.ts
@@ -0,0 +1,28 @@
+class Sorter {
+  constructor(public collection: number[] | string) {}
+
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // @TypeGuard
+        // This block is for numbers.
+        if (this.collection instanceof Array) {
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+        // This block is for strings.
+        if (typeof this.collection === "string") {
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([0, -13, -342, 34, 5345]);
+sorter.sort();
+
+console.log(sorter.collection);
